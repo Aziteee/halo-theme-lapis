ac343fa4046d714004e5e7058cc5f4cb8662c4f1
Aziteee <aziteee@outlook.com>
2024-11-08 17:56:52 +0800

    feat: support automatic scaling of images based on screen size

--- src/main.ts
+++ src/main.ts
@@ -11,3 +11,33 @@ export function init() {
     }
   });
 }
+
+export function enableResizeImage() {
+  // Resize images automatically, set width to 80%, or 100% for small screens
+  function resizeImage(): void {
+    const images: NodeListOf<HTMLImageElement> = document.querySelectorAll(".write img");
+    const screenWidth: number = window.innerWidth;
+
+    images.forEach((img: HTMLImageElement) => {
+      img.removeAttribute("width");
+      img.removeAttribute("height");
+
+      const aspectRatio: number = img.naturalHeight / img.naturalWidth;
+      if (aspectRatio > 1.5) {
+        img.style.height = "300px";
+      } else {
+        if (screenWidth > 768) {
+          img.style.width = "80%";
+        } else {
+          img.style.width = "100%";
+        }
+      }
+
+      // Center the image
+      (img.parentElement as HTMLParagraphElement).style.textAlign = "center";
+    });
+  }
+  resizeImage();
+
+  window.addEventListener("resize", resizeImage);
+}
