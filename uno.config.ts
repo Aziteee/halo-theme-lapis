df7d07c7d1356609b8f1c8fb03516e46dc584418
Aziteee <aziteee@outlook.com>
2024-11-08 16:36:04 +0800

    feat: change English font to Cantarell and change Chinese title font to Source Han Serif

--- uno.config.ts
+++ uno.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     [/^borderc-(.+)$/, ([, colorName]) => ({ "border-color": `var(--${colorName}-color)` })],
     [/^textc-(.+)$/, ([, colorName]) => ({ color: `var(--${colorName}-color)` })],
     [/^(.+)\$(.+)$/, ([, property, colorName]) => ({ [property]: `var(--${colorName}-color)` })],
-    ["font-zhongsong", { "font-family": '"STZhongsong"' }],
+    ["font-source-han-serif", { "font-family": "Cantarell,Lora,source-han-serif-tc,serif" }],
     [/^size-(\d+)$/, ([, d]) => ({ width: `${Number(d) / 4}rem`, height: `${Number(d) / 4}rem` })],
   ],
 });
